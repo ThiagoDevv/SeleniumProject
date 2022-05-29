bdaaf645a6a68776acc6c73557b22f6c657032d8
thiago fonseca <thiagofon2904@gmail.com>
2022-05-29 20:17:25 -0300

    Selenium Project

---
+++ index.js
@@ -0,0 +1,28 @@
+const {Builder, By, Key, util} = require("selenium-webdriver");
+async function gallery() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.nasa.gov/");
+    await driver.findElement(By.name("query")).sendKeys("galaxy", Key.RETURN);
+    const butao = await driver.findElement(By.linkText("Images"));
+    butao.click();
+}
+
+async function missions() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.nasa.gov/");
+    const butao = await driver.findElement(By.linkText("Missions"));
+    butao.click();
+
+    const Artemis = await driver.findElement(By.linkText("Artemis I"));
+    Artemis.click();
+}
+
+async function nasaTv() {
+    let driver = await new Builder().forBrowser("chrome").build();
+    await driver.get("https://www.nasa.gov/");
+    const butao = await driver.findElement(By.linkText("NASA TV"));
+    butao.click();
+}
+gallery();
+missions();
+nasaTv();
